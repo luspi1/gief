cd7a76516012ccc2695546d3c6314dd4cdf600bf
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-06-27 11:20:27 +0300

    v2.6.5 правка header через скрипты

--- src/js/components/changeLinkSlider.js
+++ src/js/components/changeLinkSlider.js
@@ -1,19 +1,32 @@
 //изменение ссылки в кнопке слайдера в зависимости от слайда
 
-const swiperDataLink = document.querySelectorAll('.header__slider-caption .swiper-slide')
+const swiperDataLinks = document.querySelectorAll('.header__slider-caption .swiper-slide')
 const captionSwiperBtn = document.querySelector('.caption-swiper__btn')
 
-const changeLinkSlider = () => {
-  swiperDataLink.forEach(element => {
-    if (element.classList.contains('swiper-slide-active')) {
-      location.href = element.dataset.link
-    }
-  })
-}
 
-captionSwiperBtn.addEventListener('click', () => {
-  changeLinkSlider()
-})
+if (swiperDataLinks) {
+  const changeLinkSlider = () => {
+    swiperDataLinks.forEach(element => {
+      if (element.classList.contains('swiper-slide-active')) {
+        location.href = element.dataset.link
+      }
+    })
+  }
+
+  captionSwiperBtn.addEventListener('click', () => {
+    changeLinkSlider()
+  })
+
+  const mediaQuery = window.matchMedia('(max-width: 768px)')
+
+  if (mediaQuery.matches) {
+    swiperDataLinks.forEach(el => {
+      el.addEventListener('click', (e) => {
+        location.href = e.currentTarget.dataset.link
+      })
+    })
+  }
+}
 
 
 
